b85e979bda16f4cd9fd799485771588a0cd9300d
Chintan Kotadia <chintankotadia13@gmail.com>
2020-08-26 20:12:07 +0530

    OD-14839 [#1] Included images directory from root dir, Updated styles of all page's header, TOC menu and main content

--- src/js/documentation.js
+++ src/js/documentation.js
@@ -1,5 +1,26 @@
 $(document).ready(function() {
 	// $('.book #header > #toc').prepend("<gcse:search></gcse:search>");
+  $('body').addClass('page-loaded');
+
+  $('#header').before('\
+    <header class="navbar">\
+      <div class="container clearfix">\
+        <div class="navbar-header">\
+          <a href="/" class="navbar-brand">\
+            <img src="images/logo.png" alt="logo">\
+          </a>\
+        </div>\
+      </div>\
+    </header>\
+  ');
+
+  $('.book > #header').wrap('<div id="top-header" />');
+  $('body #content').wrap('<div id="main-container" class="clearfix container" />');
+
+  var toc = $('#header #toc');
+  if (toc) {
+    $('#main-container').append('<div class="sidebar-toc"><div id="toc" class="toc2">' + toc.clone().html() + '</div> </div>');
+  }
 
   $('#toc ul').addClass('nav');
   $('#toc ul li a').addClass('nav-link');
@@ -7,8 +28,24 @@ $(document).ready(function() {
   $('body').scrollspy({ target: "#toc" });
 
   $(window).on('scroll', function() {
+    var scrollTop = $(this).scrollTop();
+
     $('#toc .hasChildren').removeClass('toc-open');
     $($('#toc .hasChildren .nav-link.active').parents('li.hasChildren:not(.toc-open)').find('.toc-expand')).trigger('click');
+
+    var mainContainer = $('#main-container');
+    var sidebarToc = $('.sidebar-toc');
+    if (mainContainer && sidebarToc && sidebarToc[0].offsetHeight > 0) {
+      var mainContainer_offsetTop = mainContainer[0].offsetTop;
+      
+      if (scrollTop > mainContainer_offsetTop) {
+        $('body').addClass('affix-toc-sidebar');
+      } else {
+        $('body').removeClass('affix-toc-sidebar');
+      }
+    } else {
+      $('body').removeClass('affix-toc-sidebar');
+    }
   });
 
   $('head').append('<style type="text/css">.gsc-adBlock, .gsc-resultsHeader, .gcsc-branding {display: none !important;}</style>');
